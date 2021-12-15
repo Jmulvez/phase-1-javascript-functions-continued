c88e4abbd8077f63c543e5c4244232f84f38b972
John <johnmulvey48@yahoo.com>
2021-12-15 10:48:27 -0500

    git@github.com:Jmulvez/phase-1-javascript-functions-continued.git

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// code your solution here
+function(mondayWork) {
+    console.log('This Monday, I will go to the office');
+  }
+function(saturdayFun) {
+    console.log('roller-skate')
+}
+const storedFunction = outer('dedicated programmer')
+function outer() {
+    const innerFunction = function (special) {
+        return '${greting}, ${youAre}, ${storedFunction}'
+    }
+}
